761bcd54965eb602947b20c0fa3fe362fd40771f
Jeffrey Rose <jtr219@outlook.com>
2022-02-08 16:28:21 -0500

    set up routes

---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require("express");
+
+const main = async () => {
+  /* -------------------------------------------------------------------------- */
+  /*                               initialize app                               */
+  /* -------------------------------------------------------------------------- */
+  const app = express();
+
+  // set up body parser
+  app.use(express.urlencoded({ extended: true }));
+
+  // set up public assets
+  app.use(express.static("public"));
+
+  /* -------------------------------------------------------------------------- */
+  /*                                set up routes                               */
+  /* -------------------------------------------------------------------------- */
+
+  /* -------------------------------------------------------------------------- */
+  /*                                SHOW POKEMON                                */
+  /* -------------------------------------------------------------------------- */
+  //   Index
+  app.get("/", (req, res) => {
+    res.send("show pokedex / poketeam");
+  });
+
+  // Show individual pokemon
+  app.get("/pokemon/:id", (req, res) => {
+    res.send("show individual pokemon");
+  });
+
+  /* -------------------------------------------------------------------------- */
+  /*                             ADD POKEMON TO TEAM                            */
+  /* -------------------------------------------------------------------------- */
+  // New
+  // submit form to add new pokemen to team
+  app.post("/team/add/:id", (req, res) => {
+    console.log("pokemon added to team");
+    res.redirect("/");
+  });
+
+  /* -------------------------------------------------------------------------- */
+  /*                         UPDATE POKEMON TEAM MEMBER                         */
+  /* -------------------------------------------------------------------------- */
+  // Edit
+  // GET /pokemon/:id/edit
+  app.get("/team/edit/:id", (req, res) => {
+    res.send("edit form for pokemon team member (but not for pokedex)");
+  });
+
+  // Update
+  // PUT /pokemon/:id
+  // form submission to edit pokemon team member
+  app.put("/team/edit/:id", (req, res) => {
+    console.log("submit update to team member");
+    res.redirect("/");
+  });
+
+  /* -------------------------------------------------------------------------- */
+  /*                         REMOVE POKEMON TEAM MEMBER                         */
+  /* -------------------------------------------------------------------------- */
+  // Destroy
+  // DELETE /pokemon/:id
+  // from submission to remove pokemon from team
+  app.delete("/team/delete/:id", (req, res) => {
+    console.log("team member removed");
+    res.redirect("/");
+  });
+
+  app.listen(3000, () => {
+    console.log("server listening on port 3000");
+  });
+};
+
+/* -------------------------------------------------------------------------- */
+/*                                launch server                               */
+/* -------------------------------------------------------------------------- */
+main().catch((err) => console.log(err));
