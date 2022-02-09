89ad53ae154b2725072464d46ee5bbc6d3586a60
Jeffrey Rose <jtr219@outlook.com>
2022-02-08 21:52:32 -0500

    update seed data to include images

--- models/Pokemon.js
+++ models/Pokemon.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const pokemonSchema = new mongoose.Schema({
   id: { type: String, required: true },
   name: { type: String, required: true },
-  //img: { type: String, required: true },
+  img: { type: String, required: true },
   type: [{ type: String, required: true }],
   stats: {
     hp: { type: String, required: true },
--- models/seed.js
+++ models/seed.js
@@ -2,14 +2,35 @@ const mongoose = require("mongoose");
 const pokedata = require("./pokedexData");
 const pokemon = require("./Pokemon");
 
-// connect to mongoose
-mongoose.connect("mongodb://localhost:27017/pokedex", () => {
-  console.log("mongoose connected");
-});
+const seedDatabase = async () => {
+  await mongoose.connect("mongodb://localhost:27017/pokedex");
+  console.log("connected to mongoose");
 
-// insert all pokemon into pokedex collection
-pokemon.Pokedex.insertMany(pokedata);
+  // clear any previous data
+  await pokemon.Pokedex.deleteMany({});
+  await pokemon.Pokedex.find().then((pk) =>
+    console.log("previous pokedex data reset: " + (pk.length === 0))
+  );
+  await pokemon.MyTeam.deleteMany({});
+  await pokemon.MyTeam.find().then((pk) =>
+    console.log("previous team data reset: " + (pk.length === 0))
+  );
 
-// insert charmander to our starting team
-const charmander = pokedata.find((pokemon) => pokemon.name === "Charmander");
-pokemon.MyTeam.insertMany([charmander]);
+  // insert all pokemon into pokedex collection
+  await pokemon.Pokedex.insertMany(pokedata);
+  await pokemon.Pokedex.find().then((pk) =>
+    console.log("All 151 pokemon are in the DB: " + (pk.length === 151))
+  );
+
+  // insert Charmander to our starting team
+  const charmander = pokedata.find((pokemon) => pokemon.name === "Charmander");
+  await pokemon.MyTeam.insertMany([charmander]);
+  await pokemon.MyTeam.findOne().then((pk) =>
+    console.log("Charmander is in your team: " + (pk.name === "Charmander"))
+  );
+
+  await mongoose.disconnect();
+  console.log("mongoose disconnected");
+};
+
+seedDatabase().catch((err) => console.log(err));
